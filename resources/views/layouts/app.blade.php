918c50272ac622d1ab29071662a94bd11887aa63
Raul Ordonez Garcia <raulog1989@gmail.com>
2017-11-12 18:19:35 +0100

    Added style to links, removed unnecessary html tags

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,58 +31,19 @@
 <div id="app">
     <nav class="navbar">
         <div class="container">
-            <div class="navbar_header">
+            <!-- Branding Image -->
+            <a class="link link--logo" href="{{ url('/') }}">
+                {{ config('app.name', 'Laravel') }}
+            </a>
 
-                <!-- Branding Image -->
-                <a class="link" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
+            <!-- User actions -->
+            @if(\Illuminate\Support\Facades\Auth::check())
 
-                <!-- User actions -->
-                @if(\Illuminate\Support\Facades\Auth::check())
-
-                        <!-- Pods -->
-                <a class="navbar-text" href="/pods">
-                    Pods
-                </a>
-                @endif
-            </div>
-
-            <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                <!-- Left Side Of Navbar -->
-                <ul class="nav navbar-nav">
-                    &nbsp;
-                </ul>
-
-                <!-- Right Side Of Navbar -->
-                <ul class="nav navbar-nav navbar-right">
-                    <!-- Authentication Links -->
-                    @guest
-                    <li><a href="{{ route('login') }}">Login</a></li>
-                    <li><a href="{{ route('register') }}">Register</a></li>
-                    @else
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                {{ Auth::user()->name }} <span class="caret"></span>
-                            </a>
-
-                            <ul class="dropdown-menu" role="menu">
-                                <li>
-                                    <a href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        Logout
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        {{ csrf_field() }}
-                                    </form>
-                                </li>
-                            </ul>
-                        </li>
-                        @endguest
-                </ul>
-            </div>
+            <!-- Pods -->
+            <a class="link link--button" href="{{ url('/pods') }}">
+                Pods
+            </a>
+            @endif
         </div>
     </nav>
 
