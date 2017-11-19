fe389cc52761fa896f0f0051f34e70d454c2d16b
Raul Ordonez Garcia <raulog1989@gmail.com>
2017-11-19 01:25:14 +0100

    Overview no longer has a mission inbound list of buttons

--- resources/views/toolbox/overview.blade.php
+++ resources/views/toolbox/overview.blade.php
@@ -12,22 +12,5 @@
                 <li style="font-size:16px;color:white;list-style-type: none;">Mission {{$mission->id}} - Pod count: {{$mission->pods->count()}}, Alien count: {{$mission->aliens->count()}}</li>
             @endforeach
         </ul>
-        <form action="/missions" method="post">
-            {{ csrf_field() }}
-            <div class="toolbox__panel toolbox__panel--triple">
-                <button class="button toolbox__button" type="button" data-value="mission-inbound/abduction">ABDUCTION SITE</button>
-                <button class="button toolbox__button" type="button" data-value="mission-inbound/crash">CRASH SITE</button>
-                <button class="button toolbox__button" type="button" data-value="mission-inbound/landing">LANDING SITE</button>
-                <button class="button toolbox__button" type="submit" name="type" value="terror">TERROR SITE</button>
-                <button class="button toolbox__button" type="button" data-value="mission-inbound/council">COUNCIL MISSION</button>
-
-                {{--<button class="button toolbox__command">--}}
-                {{--BASE ASSAULT--}}
-                {{--</button>--}}
-                {{--<button class="button toolbox__command">--}}
-                {{--BASE DEFENSE--}}
-                {{--</button>--}}
-            </div>
-        </form>
     </div>
 @endsection
