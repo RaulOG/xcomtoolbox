7cf2ae20dd318bd33e9e7931573300f0aef960b7
HectorOrdonez <hjavierog@gmail.com>
2018-02-03 16:53:33 +0100

    Fixed issue in which some images were not copied

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,4 +14,5 @@ let mix = require('laravel-mix');
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
 
+mix.copy('resources/assets/images/alien_types', 'public/images/alien_types');
 mix.copy('resources/assets/images/*', 'public/images/');
